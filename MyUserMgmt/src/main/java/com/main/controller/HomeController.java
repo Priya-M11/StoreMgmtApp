75cb6353bbde122cacc3a7b653c40760b39174f5
Priya-M11 <priyamhanare@gmail.com>
2025-03-03 19:59:45 +0530

    push project MyUserMgmt

---
+++ MyUserMgmt/src/main/java/com/main/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/testApi")
+	public String ontest()
+	{
+		return "working";
+	}
+	
+}
